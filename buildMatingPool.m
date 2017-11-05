4af1c1adbad7bc3f30eb7beac99a926dbc8072e3
Michael <Michael@10.202.201.10>
2017-11-05 10:06:36 -0700

    Minor edits relating to the lottery system.

--- buildMatingPool.m
+++ buildMatingPool.m
@@ -2,6 +2,19 @@ function matingPool = buildMatingPool(population)
 %takes in a population and builds a mating pool based on the fitness of
 %each member of the population
 
+
+%Using a "lottery" based system to decide an individual population member's
+%chance of entering the mating pool: 
+
+%Mating factor will be decided later. 
+
+ ticketVector = (fitness).*(mating factor)
+
+ %ticketVector is a numerical vector indicating the number of "tickets"
+ %each population memeber will have in the "lottery" system. 
+ 
+ 
+
 matingPool = cell(1,2);
 matingPool{1,1} = 'string1';
 matingPool{1,2} = 'string2';
