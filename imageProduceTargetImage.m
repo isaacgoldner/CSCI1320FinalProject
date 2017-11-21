7460e492bd5158c04cc0c6dd5df8e887dcb88a0e
Michael <michael.martinson@colorado.edu>
2017-11-21 15:40:40 -0700

    Started progress on making this script work.

--- imageProduceTargetImage.m
+++ imageProduceTargetImage.m
@@ -1,17 +1,33 @@
-%script to generate the target image
+%Script used to run the image evolution process: 
 
-%define the target image
+%Read in the desired target image: 
+targetImage = imread('36x36monalisa.jpg');
 
 %call the function to generate the initial random population
 imagePopulation = imageBuildPopulation(targetImage); 
 
+%specify the maximum number of generations that will be run through if the
+%target phrase has still not been produced yet
+maxGenerations = 1000;
+
+%Initialize the vectors that will be used to store data from each
+%generation, along with the cell array that will store the best image from
+%each generation: 
+maxFitness = zeros(1,maxGenerations);
+avgFitness = zeros(1,maxGenerations);
+geneticDiversity = zeros(1,maxGenerations);
+bestPhrase = char(maxGenerations,length(targetPhrase));
+bestImage = cell(maxGenerations,1); 
+
+%declare and initialize the 'generation' variable that will be used to
+%check if 'maxGenerations' has been reached; the variable 'generation' is
+%also used as a counter variable throughout the while-loop
+generation = 1;
+
 %Start timer to determine how long the evolution process will take: 
 tic;
 
-%C is used to indicate how a generator counter might be implemented: 
-c = 1; 
-while %the target image has yet to occur as an organism in the generated 
-    %populations:
+while (generation ~= maxGenerations+1) && (~ismember()      ) 
 
     %Calculate the fitness of each organism with the calculate fitness
     %function (there are to be multiple versions for this function):
@@ -52,8 +68,7 @@ while %the target image has yet to occur as an organism in the generated
     averageFitnessVec[1,c] = averageFitness;
     GeneticDiversityVec[1,c] = GeneticDiversity;
     
-    %increase generation iterator: 
-    c = c + 1; 
+  
 end
 %while loop will finally terminate when the target image is produced. 
 
