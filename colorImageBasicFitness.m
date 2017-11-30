d92b3db75c24713c7759e68d18a2f8852e77dab3
Michael <michael.martinson@colorado.edu>
2017-11-29 19:38:50 -0700

    Basic color fitness function altered to work with minimal loops.

--- colorImageBasicFitness.m
+++ colorImageBasicFitness.m
@@ -25,25 +25,44 @@ function fitness = colorImageBasicFitness(population,targetImage)
    Rdiff = abs(currentOrganismR - targetOrganismR); 
    Gdiff = abs(currentOrganismG - targetOrganismG);
    Bdiff = abs(currentOrganismB - targetOrganismB); 
-    
-   %Initialize the number of pixels that are considered "fit" to be 0.  
-   PixelsWithinTol = 0; 
-   %Set the fitness tolerance: 
-   tolerance = 0.05;  
    
-   %Run this loop to find out how many pixels in the image are fit. 
-   for i = 1:row*col
-       if (Rdiff(i) <= tolerance) && (Bdiff(i) <= tolerance) && (Gdiff(i) <= tolerance); 
-           PixelsWithinTol =  PixelsWithinTol + 1; 
-       end 
-       
-   end 
+   %1: Process using  NO loops: 
    
-   %Save the number of pixels that are considered fit in the organism. 
-    numPixelsWithinRange = length(PixelsWithinTol);
-    
-    %divide the calculated fitness value by the total number of pixels in
+   %Create a row*col matrix called ColorFit, where each element represents
+   %a pixel in the image. If the element is 3, the RGB layers are all
+   %within the set tolerance value. 
+   ColorFit = (Rdiff <= tolerance) + (Bdiff <= tolerance) + (Gdiff <= tolerance); 
+   
+   %Record the number of pixels that are within the tolerance range for all
+   %three of their color layers: 
+   numPixelsWithinRange = length(ColorFit == 3); 
+   
+    %divide the number of fit pixels by the total number of pixels in
     %the target image to express the image's fitness as a percentage: 
     fitness(i,1) = numPixelsWithinRange / (row * col);
-    
-end
+   
+   end 
+   
+end 
+
+%    %2: Backup Process using loops: 
+%    
+%    %Initialize the number of pixels that are considered "fit" to be 0.  
+%    PixelsWithinTol = 0; 
+%    %Set the fitness tolerance: 
+%    tolerance = 0.05;  
+%    
+%    %Run this loop to find out how many pixels in the image are fit. 
+%    for i = 1:row*col
+%        if (Rdiff(i) <= tolerance) && (Bdiff(i) <= tolerance) && (Gdiff(i) <= tolerance); 
+%            PixelsWithinTol =  PixelsWithinTol + 1; 
+%        end 
+%        
+%    end 
+%    
+%    %Save the number of pixels that are considered fit in the organism. 
+%     numPixelsWithinRange = length(PixelsWithinTol);
+%     
+%     %divide the calculated fitness value by the total number of pixels in
+%     %the target image to express the image's fitness as a percentage: 
+%     fitness(i,1) = numPixelsWithinRange / (row * col);
